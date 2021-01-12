e7c231abf0bd98eaba10304e775e104071238e7c
Joe Holden <joseph.holden@stryvesecure.com>
2021-01-12 13:51:34 +0000

    Changed the way the Telegram text is formatted and set the program up to receive a few extra coins' updates also

--- CryptoPricing.py
+++ CryptoPricing.py
@@ -3,17 +3,22 @@ import time
 from pycoingecko import CoinGeckoAPI
 
 # global variables
-bot_token = "Add your Telegram bot token here"
-chat_id = "Add your chat ID here"
+bot_token = "Add you Telegram bot token here"
+chat_id = "Add your Telegram chat ID here"
 threshold = 20000   # You will get a special message if the price goes below this number
 time_interval = 3600  # in seconds
 
 
-def get_crypto_price():
+def get_crypto_price(btcpricelist, ethpricelist, ltcpricelist, xtzpricelist, xmrpricelist):
 
     geckoAPI = CoinGeckoAPI()
-    response = geckoAPI.get_price(ids="bitcoin", vs_currencies="eur")
-    return response["bitcoin"]["eur"]
+    response = geckoAPI.get_price(ids=["bitcoin", "litecoin", "ethereum", "tezos", "monero"], vs_currencies="eur")
+    btcpricelist.append(response["bitcoin"]["eur"])
+    ltcpricelist.append(response["litecoin"]["eur"])
+    ethpricelist.append(response["ethereum"]["eur"])
+    xtzpricelist.append(response["tezos"]["eur"])
+    xmrpricelist.append(response["monero"]["eur"])
+    return btcpricelist, ltcpricelist, ethpricelist, xtzpricelist, xmrpricelist
 
 
 # fn to send_message through telegram
@@ -24,34 +29,44 @@ def send_message(chat_id, msg):
     requests.get(url)
 
 
-def format_msg(price_list):
-    pricefirst = round(price_list[0], 2)
-    pricesecond = round(price_list[1], 2)
-    diff = round(pricesecond - pricefirst, 2)
+def format_msg(pricelisting):
+    difflisting = []
+    for i in range(len(pricelisting)):
+        diff = (pricelisting[i][1] - pricelisting[i][0])
+        difflisting.append(diff)
 
-    msg = f'The price of Btc at the start was {pricefirst}€ and the price at the end was {pricesecond}€! The price of Btc changed {diff}€ during this time!'
+    msg = f'Here is your hourly crypto update:' \
+          f'\nBtc: {pricelisting[0][0]}€ -> {pricelisting[0][1]}€ = {difflisting[0]}€!' \
+          f'\nLtc: {pricelisting[1][0]}€ -> {pricelisting[1][1]}€ = {difflisting[1]}€!' \
+          f'\nEth: {pricelisting[2][0]}€ -> {pricelisting[2][1]}€ = {difflisting[2]}€!' \
+          f'\nXtz: {pricelisting[3][0]}€ -> {pricelisting[3][1]}€ = {difflisting[3]}€!' \
+          f'\nXmr: {pricelisting[4][0]}€ -> {pricelisting[4][1]}€ = {difflisting[4]}€!'
 
     return msg
 
 
 def main():
-    price_list = []
+    btcpricelist = []
+    ethpricelist = []
+    ltcpricelist = []
+    xtzpricelist = []
+    xmrpricelist = []
 
     # infinite loop
     while True:
-        price = get_crypto_price()
-        price_list.append(price)
+        pricelistings = get_crypto_price(btcpricelist, ethpricelist, ltcpricelist, xtzpricelist, xmrpricelist)
 
         # if the price falls below threshold, send an immediate msg
-        if price < threshold:
-            send_message(chat_id=chat_id, msg=f'BTC Price Drop Alert: {price}')
+        """if price < threshold:
+            send_message(chat_id=chat_id, msg=f'BTC Price Drop Alert: {price}')"""
 
         # send last 6 btc price
-        if len(price_list) >= 2:
-            msg = format_msg(price_list)
+        if len(pricelistings[0]) >= 2:
+            msg = format_msg(pricelistings)
             send_message(chat_id=chat_id, msg=msg)
             # empty the price_list
-            del price_list[0]
+            for i in range(len(pricelisting)):
+                del pricelisting[i][0]
 
         # fetch the price for every dash minutes
         time.sleep(time_interval)
